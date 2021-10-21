8af91469544dea04945355938747a2b8f3f2e053
Jorge Manrubia <jorge@hey.com>
2021-10-21 13:56:41 +0200

    Serialize tracks_count!

--- lib/mass_encryption/batch_serializer.rb
+++ lib/mass_encryption/batch_serializer.rb
@@ -8,11 +8,12 @@ class MassEncryption::BatchSerializer < ActiveJob::Serializers::ObjectSerializer
       "klass" => batch.klass.name,
       "from_id" => batch.from_id,
       "size" => batch.size,
-      "track" => batch.track || 0
+      "track" => batch.track || 0,
+      "tracks_count" => batch.tracks_count || 1
     )
   end
 
   def deserialize(hash)
-    MassEncryption::Batch.new(klass: hash["klass"].constantize, from_id: hash["from_id"], size: hash["size"], track: hash["track"])
+    MassEncryption::Batch.new(klass: hash["klass"].constantize, from_id: hash["from_id"], size: hash["size"], track: hash["track"], tracks_count: hash["tracks_count"])
   end
 end
